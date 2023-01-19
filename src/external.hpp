1a64b14963777557d925cb40f1b87b83190c09d8
Sofia <sofiacerasuoli@gmail.com>
2023-01-19 21:14:03 +0100

    Added licensing information

--- src/external.hpp
+++ src/external.hpp
@@ -1,4 +1,19 @@
 #pragma once
+// Copyright 2023 Sofia Cerasuoli (@SwitchAxe)
+/*
+  This file is part of Rewind.
+  Rewind is free software: you can redistribute it and/or modify it
+  under the terms of the GNU General Public License as published by the
+  Free Software Foundation, either version 3 of the license, or (at your
+  option) any later version.
+  Rewind is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+  FITNESS FOR A PARTICULAR PURPOSE.
+  See the GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along with
+  Rewind. If not, see <https://www.gnu.org/licenses/>.
+*/
 #include "evaluator.hpp"
 #include <cstdlib>
 #include <cstring>
